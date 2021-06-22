600a0c03db55714dcde5d03e2c3b43b3258aa35c
Nicholas Walton <ngwalton@gmail.com>
2021-06-22 12:04:13 -0500

    First commit; skeleton files
    Initialized nmp, installed express, set index.js to serve files from
    public, put mostly empty .js, .css, and .html files in public.

---
+++ index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set the GET route for the root
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`);
+});
+
+// set listener
+const port = 8000;
+app.listen(port, () => {
+    console.log(`Application listening on port ${port}`);
+});
